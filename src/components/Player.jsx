eb169b1dd4b0e4aadf21dcb29f63707ae14d45d1
GSouzaT <tsleirbag@gmail.com>
2024-05-01 14:05:29 -0300

    use Navigate component to redirect when the url is invalid

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,56 +1,61 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, Navigate } from "react-router-dom";
 import usePlayer from "../hooks/usePlayer";
 import Loading from "./Loading";
+import React from "react";
 
 export default function Player() {
   const { playerId } = useParams();
 
   const { response: player, loading } = usePlayer(playerId);
 
-  if (loading) return <Loading />;
+  let body;
 
-  if (!player) {
-    return null;
+  if (loading) {
+    body = <Loading />;
+  } else if (!player) {
+    body = <Navigate to="/players" />;
+  } else {
+    body = (
+      <React.Fragment>
+        <img
+          className="avatar"
+          src={`${player.avatar}`}
+          alt={`Avatar for ${player.name}`}
+        />
+        <h1 className="medium-header">{player.name}</h1>
+        <h3 className="header">#{player.number}</h3>
+        <div className="row">
+          <ul className="info-list" style={{ marginRight: 80 }}>
+            <li>
+              Team
+              <div>
+                <Link to={`/${player.teamId}`}>
+                  {player.teamId[0].toUpperCase() + player.teamId.slice(1)}
+                </Link>
+              </div>
+            </li>
+            <li>
+              Position<div>{player.position}</div>
+            </li>
+            <li>
+              PPG<div>{player.ppg}</div>
+            </li>
+          </ul>
+          <ul className="info-list">
+            <li>
+              APG<div>{player.apg}</div>
+            </li>
+            <li>
+              SRG<div>{player.srg}</div>
+            </li>
+            <li>
+              RPG<div>{player.rpg}</div>
+            </li>
+          </ul>
+        </div>
+      </React.Fragment>
+    );
   }
 
-  return (
-    <div className="panel">
-      <img
-        className="avatar"
-        src={`${player.avatar}`}
-        alt={`Avatar for ${player.name}`}
-      />
-      <h1 className="medium-header">{player.name}</h1>
-      <h3 className="header">#{player.number}</h3>
-      <div className="row">
-        <ul className="info-list" style={{ marginRight: 80 }}>
-          <li>
-            Team
-            <div>
-              <Link to={`/${player.teamId}`}>
-                {player.teamId[0].toUpperCase() + player.teamId.slice(1)}
-              </Link>
-            </div>
-          </li>
-          <li>
-            Position<div>{player.position}</div>
-          </li>
-          <li>
-            PPG<div>{player.ppg}</div>
-          </li>
-        </ul>
-        <ul className="info-list">
-          <li>
-            APG<div>{player.apg}</div>
-          </li>
-          <li>
-            SRG<div>{player.srg}</div>
-          </li>
-          <li>
-            RPG<div>{player.rpg}</div>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
+  return <div className="panel">{body}</div>;
 }
